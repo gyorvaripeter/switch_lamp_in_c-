c62d7c7842200b06b0a0c662feab8cb58cadb726
Győrvári Péter <gyorvaripeter1996@gmail.com>
2019-11-21 00:29:25 +0100

    lampakapcsolgatasok

--- switch_lamp_in_c++/main.cpp
+++ switch_lamp_in_c++/main.cpp
@@ -1,15 +1,19 @@
-//
-//  main.cpp
-//  switch_lamp_in_c++
-//
-//  Created by Győrvári Péter on 2019. 11. 20..
-//  Copyright © 2019. Győrvári Péter. All rights reserved.
-//
-
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int l [4], i=00;
+    cout << "write 4  switch position, off=0 on=1\n" << endl;
+    while(i<4){
+        cin>>l[i];
+        i++;
+    }
+    sort(l,l+4);
+    switch(l[0]+l[1]){
+        case 0: cout << "darkness reigns" << endl;break;
+        default: cout << "The light is shining" << endl;break;
+    }
+        
+        
+    
     return 0;
 }
