34a82e5115854abe56acc410aaddbf166d7e4b35
Győrvári Péter <gyorvaripeter1996@gmail.com>
2019-11-25 16:42:19 +0100

    1.1

--- switch_lamp_in_c++/main.cpp
+++ switch_lamp_in_c++/main.cpp
@@ -1,16 +1,24 @@
+//
+//  masik_megoldas.cpp
+//
+//
+//  Created by Győrvári Péter on 2019. 11. 25..
 #include <iostream>
 using namespace std;
-int main(int argc, const char * argv[]) {
-    int l [4], i=0;
-    cout << "write 4  switch position, off=0 on=1\n" << endl;
-    while(i<4){
-        cin>>l[i];
-        i++;
-    }
-    sort(l,l+4);
-    switch(l[0]+l[1]){
-        case 0: cout << "darkness reigns" << endl;break;
-        default: cout << "The light is shining" << endl;break;
-    }
+int main()
+{
+    int a,b,c,d;
+    cout<<"A\tB\tC\tD\t\tlight";
+    
+    for(a=0;a<=1;++a)
+        for(b=0;b<=1;++b)
+            for(c=0;c<=1;++c)
+                for(d=0;d<=1;++d)
+            {//érdemi logikai funkció------------------------------------------------
+                if((a and b)or(c and d))
+                    cout<<"\n\n"<<a<<"\t"<<b<<"\t"<<c<<"\t"<<d<<"\t\t"<<"The light is shining"<<endl;
+                else
+                    cout<<"\n\n"<<a<<"\t"<<b<<"\t"<<c<<"\t"<<d<<"\t\t"<<"darkness reigns"<<endl;
+            }
     return 0;
 }
