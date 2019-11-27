0cebfee31de4736caedaadacaa73c244390b66e9
Győrvári Péter <gyorvaripeter1996@gmail.com>
2019-11-27 01:35:17 +0100

    hibajavítás

--- switch_lamp_in_c++/main.cpp
+++ switch_lamp_in_c++/main.cpp
@@ -9,13 +9,12 @@ int main()
 {
     int a,b,c,d;
     cout<<"A\tB\tC\tD\t\tlight";
-    
     for(a=0;a<=1;++a)
         for(b=0;b<=1;++b)
             for(c=0;c<=1;++c)
                 for(d=0;d<=1;++d)
             {//érdemi logikai funkció------------------------------------------------
-                if((a and b)or(c and d))
+                if((a and b and c)or(b and c and d) or (a and c and d) or (a and b and d))
                     cout<<"\n\n"<<a<<"\t"<<b<<"\t"<<c<<"\t"<<d<<"\t\t"<<"The light is shining"<<endl;
                 else
                     cout<<"\n\n"<<a<<"\t"<<b<<"\t"<<c<<"\t"<<d<<"\t\t"<<"darkness reigns"<<endl;
